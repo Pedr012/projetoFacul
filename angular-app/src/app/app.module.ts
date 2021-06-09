adca903e22aceead6c9826f6853d68e8465bd4e5
Caio Rodrigo <cacodigo@gmail.com>
2021-06-08 22:26:34 -0300

    Adicionando o componente da navbar

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConsultComponent } from './template/consult/consult.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, CabecalhoComponent, ConsultComponent],
+  declarations: [AppComponent, RegisterComponent, CabecalhoComponent, ConsultComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule, BrowserAnimationsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
