2e4a136280f6f4f3393b35572f239a3d87a41636
Pedro Rocha <pedrohrrf17@gmail.com>
2021-06-09 22:39:26 -0300

    Rotas add

--- angular-app/src/app/components/navbar/navbar.component.ts
+++ angular-app/src/app/components/navbar/navbar.component.ts
@@ -3,20 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-
 export class NavbarComponent {
-  public online = false
+  public online = false;
 
   handleMenu() {
-    const menu = document.querySelector("#menu") as HTMLDivElement
-    const right = !!menu.style.right 
-      ? menu.style.right 
-      : "-20rem"
-    
-    menu.style.right = right === "0px" || right === ""
-      ? "-20rem"
-      : "0px"
+    const menu = document.querySelector('#menu') as HTMLDivElement;
+    const right = !!menu.style.right ? menu.style.right : '-20rem';
+
+    menu.style.right = right === '0px' || right === '' ? '-20rem' : '0px';
   }
 }
